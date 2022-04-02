77a5cb2b5b79ce4e9bb9572fb86593c74ee965bb
Aviraj Newatia <projectavi.exe@gmail.com>
2022-04-02 14:53:54 -0400

    Update Construct.svelte
    parsing and etc. works now less go

--- src/Construct.svelte
+++ src/Construct.svelte
@@ -1,4 +1,11 @@
 <script>
+
+    function makeTree(prompt) {
+        return {"topic": prompt,
+                "children": [],
+                "completed": false};
+    };
+
     import { Configuration, OpenAIApi } from 'openai';
     import axios from "axios";
 import { listen } from 'svelte/internal';
@@ -18,10 +25,10 @@ import { listen } from 'svelte/internal';
     // Write a function to take the prompt and send it to the node server and await a response
 
     async function getList() {
-        const request = "Make me a list of topics to learn " + prompt + " in array format"; 
+        let request = "Make me a list of topics to learn " + prompt; 
         // console.log(prompt);
 
-        let completion = await openai.createCompletion('text-curie-001', {
+        let completion = await openai.createCompletion('text-davinci-002', {
         prompt: request,
 
         // playground settings
@@ -41,19 +48,22 @@ import { listen } from 'svelte/internal';
 
     let list_list = result.split("\n");
     let i = 2
+    let rep = 0;
     while (i < list_list.length) {
         // console.log(list_list[i]);
         // console.log(list_list[i].charAt(0), list_list[i].charAt(1), list_list[i].charAt(0) == String("-"));
         if (list_list[i].charAt(0) == "-"){
             list_list[i] = list_list[i].slice(1);
-            console.log(list_list);
+            // console.log(list_list);
             i++;
         }else if (list_list[i].charAt(1) == "."){
             list_list[i] = list_list[i].slice(2);
-            console.log(list_list);
+            // console.log(list_list);
             i++;
         }else{
-            completion = await openai.createCompletion('text-curie-001', {
+            rep ++;
+            console.log(request);
+            completion = await openai.createCompletion('text-davinci-002', {
                 prompt: request,
 
                 // playground settings
@@ -64,12 +74,90 @@ import { listen } from 'svelte/internal';
                 presence_penalty: 0
             })
             result = completion.data.choices[0]["text"];
+            // console.log(result);
             i = 2
             console.log("its been reset")
+            if (rep >= 2) {
+                console.log("end");
+                i = list_list.length;
+            }
         }
     }
 
-    return list_list;
+    let topicTree = makeTree(prompt);
+
+    list_list.shift();
+    list_list.shift();
+
+    for (let i = 0; i < list_list.length; i++){
+        topicTree["children"].push(makeTree(list_list[i]));
+    }
+
+    for (let k = 0; k < topicTree["children"].length; k++){
+        let request = "Make me a list of topics to learn " + topicTree["children"][k]["topic"]; 
+        // console.log(prompt);
+
+        let completion = await openai.createCompletion('text-davinci-002', {
+        prompt: request,
+
+        // playground settings
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0
+    })
+
+    // console.log(completion);
+    // console.log(completion.data.choices[0]["text"]);
+
+    let result = completion.data.choices[0]["text"];
+    
+    let parsed = false;
+
+    let list_list = result.split("\n");
+    let i = 2
+    while (i < list_list.length) {
+        // console.log(list_list[i]);
+        // console.log(list_list[i].charAt(0), list_list[i].charAt(1), list_list[i].charAt(0) == String("-"));
+        if (list_list[i].charAt(0) == "-"){
+            list_list[i] = list_list[i].slice(1);
+            // console.log(list_list);
+            i++;
+        }else if (list_list[i].charAt(1) == "."){
+            list_list[i] = list_list[i].slice(2);
+            // console.log(list_list);
+            i++;
+        }else{
+            completion = await openai.createCompletion('text-davinci-002', {
+                prompt: request,
+
+                // playground settings
+                temperature: 0.7,
+                max_tokens: 256,
+                top_p: 1,
+                frequency_penalty: 0,
+                presence_penalty: 0
+            })
+            result = completion.data.choices[0]["text"];
+            // console.log(result);
+            i = 2
+            console.log("its been reset")
+            if (rep >= 2) {
+                console.log("end");
+                i = list_list.length;
+            }
+        }
+    }
+
+    list_list.shift();
+    list_list.shift();
+    for (let j = 0; j < list_list.length; j++){
+        topicTree["children"][k]["children"].push(makeTree(list_list[j]));
+    }
+    }
+
+    return topicTree;
 
         // const { data } = await axios.get('http://127.0.0.1:3001/prompt', {
         //     params: { text: request }
@@ -90,6 +178,6 @@ import { listen } from 'svelte/internal';
     {#await promise}
         <h1> Waiting... </h1>
     {:then response}
-        <h1> Response: {response} </h1>
+        <h1> Response: {JSON.stringify(response)} </h1>
     {/await}
 </html>
