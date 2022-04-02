aad25b2b228d8648bb24033423dcc47f743e87cf
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2022-04-02 11:09:22 -0400

    Setup API proxy

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,15 @@ import postcss from './postcss.config.js'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  css: { postcss }
+  css: { postcss },
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
