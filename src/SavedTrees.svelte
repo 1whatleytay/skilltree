4f384764901d75c8996890adb34b619f555bab40
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2022-04-03 02:04:32 -0400

    Some skin

--- src/SavedTrees.svelte
+++ src/SavedTrees.svelte
@@ -1,119 +1,50 @@
 <script>
 	import {skillTrees} from './store.js';
-    import { Router, Route, Link } from "svelte-navigator";
+  import { Router, Route, Link } from "svelte-navigator";
 
-    console.log(1);
+  console.log(1);
 
-    let activeObj = $skillTrees;
+  let activeObj = $skillTrees;
 
-    console.log($skillTrees);
-    console.log(activeObj);
+  console.log($skillTrees);
+  console.log(activeObj);
 
-    if (typeof(activeObj) == "undefined") {
-        activeObj = JSON.stringify({});
-    };
+  if (typeof(activeObj) == "undefined") {
+      activeObj = JSON.stringify({});
+  };
 
-    console.log(1);
-    console.log(activeObj);
-    console.log(JSON.parse(activeObj));
+  // I could be fuckin' something up here
+  const parsedObject = JSON.parse(activeObj)
 
-    let num_trees = Object.keys(JSON.parse(activeObj)).length;
-
-    let num_rows = Math.ceil(num_trees/4);
-    let final_row_num_cols = num_trees % 4;
-
-    let layout = [];
-
-    for (let i = 0; i < num_rows; i++) {
-        let row = [];
-        if (i == num_rows - 1) {
-            for (let j = 0; j < final_row_num_cols; j++) {
-                let key = Object.keys(JSON.parse(activeObj))[i*4 + j];
-                console.log(typeof(key));
-                // console.log("keys: " + JSON.stringify(Object.keys(JSON.parse(activeObj))));
-                let value = JSON.parse(activeObj)[key];
-                console.log([key, value]);
-                row.push([key, value]);
-            }
-        } else {
-            for (let j = 0; j < 4; j++) {
-                let key = Object.keys(JSON.parse(activeObj))[i*4 + j];
-                let value = JSON.parse(activeObj)[key];
-                row.push([key, value]);
-            }
-        }
-        layout.push(row);
-    }
-
-    console.log(layout);
+  let layout = Object.keys(parsedObject);
 </script>
 
-<div class="flex flex-wrap items-center justify-center mt-32">
-    <Link to="/">
-        <button class="mr-12 w-24 bg-gray-200 px-4 py-2 my-4 rounded-lg">
-            &#x2039; Back
-        </button>
-    </Link>
+<!--<div class="flex flex-wrap items-center justify-center mt-32">-->
+<!--    <Link to="/">-->
+<!--        <button class="mr-12 w-24 bg-gray-200 px-4 py-2 my-4 rounded-lg">-->
+<!--            &#x2039; Back-->
+<!--        </button>-->
+<!--    </Link>-->
 
-    <div class="text-center text-gray-700 text-4xl lg:text-8xl">
+<!--    <div class="text-center text-gray-700 text-6xl">-->
+<!--        Saved Skilltrees-->
+<!--    </div>-->
+
+<div class="bg-green-100 rounded-lg w-auto max-w-4xl mx-auto uppercase font-bold flex items-center p-8 m-8">
+    <div class="text-4xl text-gray-800">
+        <Link to="/" class="font-mono text-sm text-gray-600 hover:text-gray-500"> &#x2039; Back /</Link>
         Saved Skilltrees
     </div>
 </div>
+<!--</div>-->
 
-<div class="justify-center mt-32">
-    {#each layout as row}
-        <div class="row mt-4">
-            {#each row as cell}
-                <Link to="/skilltree/{cell[0]}">
-                    <div class="column mt-6">
-                        <div class="card"> {cell[0]} </div>
-                    </div>
-                </Link>
-            {/each}
-        </div>
+<div class="max-w-4xl mx-auto flex flex-wrap mt-32">
+    {#each layout as cell}
+        <Link to="/skilltree/{cell}">
+            <div class="bg-green-100 rounded-lg w-64 h-32 p-6 uppercase text-gray-800 font-medium text-xl flex flex-col mt-4 mx-4">
+                <div>{cell}</div>
+                <div class="text-gray-600 text-sm mt-auto">Learn &rarr;</div>
+            </div>
+        </Link>
     {/each}
 </div>
-
-<style>
-    * {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-/* Float four columns side by side */
-.column {
-  float: left;
-  width: 25%;
-  padding: 0 10px;
-}
-
-/* Remove extra left and right margins, due to padding in columns */
-.row {margin: 0 -5px;}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Style the counter cards */
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
-  padding: 16px;
-  text-align: center;
-  background-color: #f1f1f1;
-}
-
-/* Responsive columns - one column layout (vertical) on small screens */
-@media screen and (max-width: 600px) {
-  .column {
-    width: 100%;
-    display: block;
-    margin-bottom: 20px;
-  }
-}
-</style>
